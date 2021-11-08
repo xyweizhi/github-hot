dac06a8b50442c20a38b32bf380f9646e7416b91
caizhuanxi <caizhuanxi@wufeng-network.com>
2021-11-08 20:10:06 +0800

    解决内置滚动条问题

--- src/page/hot/index.jsx
+++ src/page/hot/index.jsx
@@ -1,186 +1,199 @@
-import React from 'react'
-import {
-  Spin, Button, message,
-} from 'antd'
-import axios from 'axios'
-import InfiniteScroll from 'react-infinite-scroller'
-import Content from '@/components/content'
+import React from "react";
+import { Spin, Button, message } from "antd";
+import axios from "axios";
+import InfiniteScroll from "react-infinite-scroller";
+import Content from "@/components/content";
 
-import { parseUrl, stringify } from '@/util'
+import { parseUrl, stringify } from "@/util";
 
-import styles from './hot.less'
-
+import styles from "./hot.less";
 
 function appendParamToUrl(name, value) {
-  const url = new URL(window.location.href)
-  const [base, urlParam] = parseUrl()
+  const url = new URL(window.location.href);
+  const [base, urlParam] = parseUrl();
   if (!urlParam) {
-    url.hash = `${base}?${name}=${value}`
-    window.location.href = url.href
-    return
+    url.hash = `${base}?${name}=${value}`;
+    window.location.href = url.href;
+    return;
   }
   const urlParams = urlParam.map((val) => {
     if (val.name === name) {
-      val.value = value
+      val.value = value;
     }
-    return val
-  })
-  url.hash = `${base}?${stringify(urlParams)}`
-  window.location.href = url.href
+    return val;
+  });
+  url.hash = `${base}?${stringify(urlParams)}`;
+  window.location.href = url.href;
 }
 
 function getLanguageFromUrlParams() {
-  const [base, urlParam] = parseUrl()
+  const [base, urlParam] = parseUrl();
   if (!urlParam) {
-    return 'All'
+    return "All";
   }
-  let language = ''
+  let language = "";
   urlParam.find((val) => {
-    if (val.name === 'language') {
-      language = val.value
-      return true
+    if (val.name === "language") {
+      language = val.value;
+      return true;
     }
-    return false
-  })
-  return language
+    return false;
+  });
+  return language;
 }
 
 export default class App extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
 
     this.map = [
-      { language: 'All', url: 'https://api.github.com/search/repositories?q=stars:%3E1&sort=stars&order=desc&type=Repositories&per_page=10' },
-      { language: 'Javascript', url: 'https://api.github.com/search/repositories?q=stars:%3E1+language:javascript&sort=stars&order=desc&type=Repositories&per_page=10' },
-      { language: 'Ruby', url: 'https://api.github.com/search/repositories?q=stars:%3E1+language:ruby&sort=stars&order=desc&type=Repositories&per_page=10' },
-      { language: 'Java', url: 'https://api.github.com/search/repositories?q=stars:%3E1+language:java&sort=stars&order=desc&type=Repositories&per_page=10' },
-      { language: 'CSS', url: 'https://api.github.com/search/repositories?q=stars:%3E1+language:css&sort=stars&order=desc&type=Repositories&per_page=10' },
-    ]
+      {
+        language: "All",
+        url: "https://api.github.com/search/repositories?q=stars:%3E1&sort=stars&order=desc&type=Repositories&per_page=10",
+      },
+      {
+        language: "Javascript",
+        url: "https://api.github.com/search/repositories?q=stars:%3E1+language:javascript&sort=stars&order=desc&type=Repositories&per_page=10",
+      },
+      {
+        language: "Ruby",
+        url: "https://api.github.com/search/repositories?q=stars:%3E1+language:ruby&sort=stars&order=desc&type=Repositories&per_page=10",
+      },
+      {
+        language: "Java",
+        url: "https://api.github.com/search/repositories?q=stars:%3E1+language:java&sort=stars&order=desc&type=Repositories&per_page=10",
+      },
+      {
+        language: "CSS",
+        url: "https://api.github.com/search/repositories?q=stars:%3E1+language:css&sort=stars&order=desc&type=Repositories&per_page=10",
+      },
+    ];
 
-    const initLanguage = getLanguageFromUrlParams()
+    const initLanguage = getLanguageFromUrlParams();
 
-    let target = 0
+    let target = 0;
 
     this.map.find((v, index) => {
       if (v.language === initLanguage) {
-        target = index
-        return true
+        target = index;
+        return true;
       }
-      return false
-    })
+      return false;
+    });
 
     this.state = {
       data: [],
       target,
       loading: true,
       page: 1,
-    }
+    };
 
-    this.ref = null
+    this.ref = null;
 
-    getLanguageFromUrlParams()
+    getLanguageFromUrlParams();
 
-    this.getGithubData = this.getGithubData.bind(this)
-    this.handleInfiniteOnLoad = this.handleInfiniteOnLoad.bind(this)
+    this.getGithubData = this.getGithubData.bind(this);
+    this.handleInfiniteOnLoad = this.handleInfiniteOnLoad.bind(this);
 
-    this.source = null
+    this.source = null;
   }
 
   componentDidMount() {
-    const { target } = this.state
+    const { target } = this.state;
     // did mount, init data
-    this.getGithubData(target)
+    this.getGithubData(target);
   }
 
   componentWillUnmount() {
     if (this.source) {
-      this.source.cancel('Component will unmount, request will be canceled.')
+      this.source.cancel("Component will unmount, request will be canceled.");
     }
   }
 
   handleButtonClick(index) {
-    const { target, loading } = this.props
-    if (target === index || loading) return
-    this.getGithubData(index)
+    const { target, loading } = this.props;
+    if (target === index || loading) return;
+    this.getGithubData(index);
   }
 
   handleInfiniteOnLoad(page) {
-    const { target, loading } = this.state
-    console.log(page)
-    if (loading) return
-    this.getGithubData(target, page)
+    const { target, loading } = this.state;
+    console.log(page);
+    if (loading) return;
+    this.getGithubData(target, page);
   }
 
   getGithubData(index, pageIndex) {
-    const { page, data } = this.state
+    const { page, data } = this.state;
 
-    this.setState({ loading: true, target: index })
-    const { CancelToken } = axios
-    this.source = CancelToken.source()
-    axios.get(`${this.map[index].url}&page=${pageIndex || 1}`, {
-      cancelToken: this.source.token,
-    }).then((res) => {
-      const { items } = res.data
-      if (pageIndex > 1) {
-        this.setState({ loading: false, data: data.concat(items) })
-        appendParamToUrl('language', this.map[index].language)
-        return
-      }
-      this.setState({ loading: false, data: items })
-      appendParamToUrl('language', this.map[index].language)
-      this.source = null
-    }).catch((thrown) => {
-      if (axios.isCancel(thrown)) {
-        console.log('Request canceled', thrown.message)
-        this.source = null
-        return
-      }
-      // message.error(thrown.response ? thrown.response.data.message : thrown.message, 0)
-      window.alert(thrown.response ? thrown.response.data.message : thrown.message)
-      this.setState({loading: false})
-      this.source = null
-    })
+    this.setState({ loading: true, target: index });
+    const { CancelToken } = axios;
+    this.source = CancelToken.source();
+    axios
+      .get(`${this.map[index].url}&page=${pageIndex || 1}`, {
+        cancelToken: this.source.token,
+      })
+      .then((res) => {
+        const { items } = res.data;
+        if (pageIndex > 1) {
+          this.setState({ loading: false, data: data.concat(items) });
+          appendParamToUrl("language", this.map[index].language);
+          return;
+        }
+        this.setState({ loading: false, data: items });
+        appendParamToUrl("language", this.map[index].language);
+        this.source = null;
+      })
+      .catch((thrown) => {
+        if (axios.isCancel(thrown)) {
+          console.log("Request canceled", thrown.message);
+          this.source = null;
+          return;
+        }
+        // message.error(thrown.response ? thrown.response.data.message : thrown.message, 0)
+        window.alert(
+          thrown.response ? thrown.response.data.message : thrown.message
+        );
+        this.setState({ loading: false });
+        this.source = null;
+      });
   }
 
   render() {
-    const {
-      loading, data, target,
-    } = this.state
-
+    const { loading, data, target } = this.state;
     return (
       <div className={styles.app}>
-        <Spin
-          size="large"
-          spinning={loading}
-        />
-        <div className={styles['hot-title']}>
+        <Spin size="large" spinning={loading} />
+        <div className={styles["hot-title"]}>
           {this.map.map((value, index) => {
-            const { language } = value
+            const { language } = value;
             return (
               <Button
-                style={{ fontSize: '20px'}}
+                style={{ fontSize: "20px" }}
                 key={language}
                 loading={target === index ? loading : false}
-                type={target === index ? 'link' : 'text'}
+                type={target === index ? "link" : "text"}
                 onClick={this.handleButtonClick.bind(this, index)}
               >
                 {language}
               </Button>
-            )
+            );
           })}
         </div>
-        <div className={styles['items-content']}>
-        <InfiniteScroll
-          initialLoad={false}
-          pageStart={1}
-          loadMore={this.handleInfiniteOnLoad}
-          hasMore={!loading}
-          useWindow={false}
-        >
-          <Content data={data} />
-        </InfiniteScroll>
+        <div className={styles["items-content"]}>
+          <InfiniteScroll
+            initialLoad={false}
+            pageStart={1}
+            loadMore={this.handleInfiniteOnLoad}
+            hasMore={!loading}
+            useWindow={false}
+          >
+            <div className={styles.icontent}>
+              <Content data={data} />
+            </div>
+          </InfiniteScroll>
         </div>
       </div>
-    )
+    );
   }
 }
